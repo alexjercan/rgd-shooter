b341c90d974452ae6ad600ed594c2f602579b257
IamandeiMihai <53166011+IamandeiMihai@users.noreply.github.com>
2020-07-22 16:15:31 +0300

    Adaugat sistem de looting

---
+++ RoadToFive/Assets/_Project/Scripts/Looting/LootDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootDetails : MonoBehaviour
+{
+    public int count;
+    public bool stackable;
+    
+    public bool isWeapon;
+
+    public bool isAmmo;
+    public AmmoType ammoType;
+
+}
