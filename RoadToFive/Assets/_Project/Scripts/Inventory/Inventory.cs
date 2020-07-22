f7aec03d05e05d00b7ccda6ae7b3110f9b13e4ac
Manea Marius <ionut.manea@stud.acs.upb.ro>
2020-07-22 15:11:28 +0300

    Inventory
    Am creat un item pentru inventory care contine obiectul numarul de obiecte daca se stacheaza daca este arma, si am creat un script care retine pentru arme diferite statistici inclusiv tipul de munitie cu care trage, m-am gandit ca vine mai usor pick-upul pentru munitie daca folosim munitie pe tipuri decat specific unei arme

---
+++ RoadToFive/Assets/_Project/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class Inventory : MonoBehaviour
+{
+    public const int maxInvetory = 10;
+
+    [Serializable]
+    public class InventoryItem
+    {
+        public GameObject item;
+        public int count;
+        public bool stackable;
+        public bool isWeapon;
+        int limit;
+
+        public void InitItem()
+        {
+            item = null;
+            count = 0;
+            stackable = false;
+            isWeapon = false;
+            limit = 0;
+        }
+    }
+
+    [Serializable]
+    public class AmmoItem
+    {
+        public AmmoType ammoType;
+        public int count;
+    }
+
+    public InventoryItem[] inventory;
+    public AmmoItem[] munition;
+    public InventoryItem itemInHand;
+    public int currentItemInHand;
+
+
+    public int GetAmmoCount(AmmoType ammoType)
+    {
+        return munition[(int)ammoType].count;
+    }
+
+    void Start()
+    {
+        inventory = new InventoryItem[maxInvetory];
+        currentItemInHand = 0;
+        itemInHand = inventory[0];
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.qKey.isPressed)
+        {
+            DropItem();
+        }
+
+        ScrollItems();
+
+        UpdateCurrentWeaponAmmo();
+    }
+
+    public void DropItem()
+    {
+        itemInHand.count--;
+        if (itemInHand.count <= 0)
+        {
+            GameObject.Destroy(itemInHand.item);
+            inventory[currentItemInHand].InitItem();
+        }
+    }
+    public void PickUpItem()
+    {
+        // TODO
+    }
+
+    public void ScrollItems()
+    {
+        Vector2Control wheelInput = Mouse.current.scroll;
+
+        if (wheelInput.y.ReadValue() != 0)
+        {
+            currentItemInHand += Math.Sign(wheelInput.y.ReadValue());
+            if (currentItemInHand < 0)
+            {
+                currentItemInHand = maxInvetory - 1;
+            }
+            if (currentItemInHand >= maxInvetory)
+            {
+                currentItemInHand = 0;
+            }
+            itemInHand = inventory[currentItemInHand];
+        }
+    }
+
+    public void UpdateCurrentWeaponAmmo()
+    {
+        if (itemInHand != null && itemInHand.isWeapon)
+        {
+            itemInHand.item.GetComponent<WeaponStats>().ammoCount = GetAmmoCount(itemInHand.item.GetComponent<WeaponStats>().ammoType);
+        }
+    }
+}
+
+public enum AmmoType
+{
+    ammo1 = 0,
+    ammo2 = 1,
+    ammo3 = 2
+    // TODO: Sa ne gandim ce fel de munitie folosim
+}
---
+++ RoadToFive/Assets/_Project/Scripts/Weapons/WeaponStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : MonoBehaviour
+{
+    public int ammoCount;
+    public int rateOfFire;
+    public int damage;
+    public AmmoType ammoType;
+
+    public GameObject owner;
+
+    public bool CanShoot()
+    {
+        return ammoCount != 0;
+    }
+}
+
+
+
