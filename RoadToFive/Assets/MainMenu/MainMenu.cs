6843e710872ec9445a0191907fea9af90550d2f1
Jercan Alex <jercan_alex27@yahoo.com>
2020-08-06 11:38:34 +0300

    main menu

--- RoadToFive/Assets/MainMenu/MainMenu.cs
+++ RoadToFive/Assets/MainMenu/MainMenu.cs
@@ -1,27 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace MainMenu
 {
-    public string mainSceneClient = "MainSceneClient";
-    public GameObject mainMenuCanvas;
-    public GameObject loadingScreenCanvas;
+    public class MainMenu : MonoBehaviour
+    {
+        public string mainSceneClient = "MainSceneClient";
+        public GameObject mainMenuCanvas;
+        public GameObject loadingScreenCanvas;
 
-    private AsyncOperation _mainSceneLoading;
+        private AsyncOperation _mainSceneLoading;
     
-    public void PlayGame() 
-    {
-        mainMenuCanvas.SetActive(false);
-        loadingScreenCanvas.SetActive(true);
-        _mainSceneLoading = SceneManager.LoadSceneAsync(mainSceneClient, LoadSceneMode.Single);
-        _mainSceneLoading.completed += operation =>
+        public void PlayGame() 
         {
+            mainMenuCanvas.SetActive(false);
+            loadingScreenCanvas.SetActive(true);
+            _mainSceneLoading = SceneManager.LoadSceneAsync(mainSceneClient, LoadSceneMode.Single);
+            _mainSceneLoading.completed += operation =>
+            {
 
-        };
-    }
+            };
+        }
 
-    public void QuitGame()
-    {
-        Application.Quit();
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
     }
 }
